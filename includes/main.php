092e563b8397be5af8b99c4f8dcdbe9b58f4e015
RachidBarr <rbarr@normandiewebschool.fr>
2019-12-19 16:49:39 +0100

    remplacement du if par la condition ternaire

--- includes/main.php
+++ includes/main.php
@@ -1,1 +1,10 @@
 <?php
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 'accueil';
+}
+
+// $page = isset($GET['page']) ? $page :'accueil'; la meme chose que le if else
+require './includes/' . $page . '.php';
